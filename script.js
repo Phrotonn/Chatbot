61601fde4d7f142bab89d12ca4e5039e65e66615
Phrotonn <149646857+Phrotonn@users.noreply.github.com>
2025-07-02 21:20:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const sendBtn = document.getElementById('send-btn');
     const newChatBtn = document.getElementById('new-chat');
     const chatHistoryEl = document.getElementById('chat-history');
-    const API_URL = "https://897e-35-221-137-30.ngrok-free.app";
+    const API_URL = "https://a462-35-221-137-30.ngrok-free.app/chat";
 
     let currentChatId = null;
     let chats = {};
